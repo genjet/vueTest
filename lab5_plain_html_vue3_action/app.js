67e2803d5757001db0881647f9123c301d96df09
genjet <gengywang@gmail.com>
2024-10-01 16:16:20 +0800

    p58-60

--- lab5_plain_html_vue3_action/app.js
+++ lab5_plain_html_vue3_action/app.js
@@ -1,10 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter1: 30
+            counter1: 30,
+            todo:''
         }
     },
     methods: {
+        setTodo(event) { // assume target是一個input元件 
+            this.todo = event.target.value;
+        },
         increase1() {
             this.counter1++;
         },
@@ -12,7 +16,9 @@ const app = Vue.createApp({
             this.counter1--;
         },
         increase(step) {
-            this.counter1 = this.counter1 + step;
+            setTimeout(() => {
+                this.counter1 = this.counter1 + step;
+            }, 1000);
         },
         decrease: function (step) {
             this.counter1 = this.counter1 - step;
