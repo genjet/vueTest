39f7f53ffa43bf79ae283cd1d5938d3613573762
genjet <gengywang@gmail.com>
2024-10-04 13:51:35 +0800

    P132-135

--- lab9_plain_component_vue3/app.js
+++ lab9_plain_component_vue3/app.js
@@ -18,4 +18,27 @@ const app = Vue.createApp({
     watch: {
     }
 })
+app.component("course-content", {
+    template: `
+        <li>
+            <h2>{{course.id}}</h2>
+            <button @click="toggleCourseDetail">show detail1</button>
+            <ul v-if="detailsVisible">
+                <li>{{course.name}}</li>
+                <li>{{course.duration}}</li>
+            </ul>
+        </li>
+    `,
+    data() {
+        return {
+            course: { id: "POOP1", name: "Python OOP1", duration: "351hr" },
+            detailsVisible: true
+        }
+    },
+    methods: {
+        toggleCourseDetail() {
+            this.detailsVisible = !this.detailsVisible;
+        }
+    },
+})
 app.mount("#app9")
