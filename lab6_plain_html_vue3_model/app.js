c7ff9ffaeba1dc8700f5f5a6738afa2ff74b78ab
genjet <gengywang@gmail.com>
2024-10-04 09:53:24 +0800

    p88-89

--- lab6_plain_html_vue3_model/app.js
+++ lab6_plain_html_vue3_model/app.js
@@ -6,18 +6,29 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        outputIssue() {
-            console.log("格式化issue")
-            if (this.issue==="") {
-                return "";
-            }
-            return `[!!]${this.issue}`
-        },
         resetIssue() {
             this.issue = ""
         },
         increase() {
             this.counter += 1;
+        }
+    },
+    computed: {
+        formatIssue() {
+            console.log("[format]格式化issue")
+            if (this.issue === "") {
+                return "";
+            }
+            return `[!!]${this.issue}`
+        },
+        formatIssue_p() {
+            return function (param) {
+                console.log("[format]格式化issue:" + param)
+                if (this.issue === "") {
+                    return "";
+                }
+                return `[!!]${this.issue}`
+            }
         }
     }
 })
