5d7e6ccd8554d8e29f0d1099cfe629567a2f3cb9
genjet <gengywang@gmail.com>
2024-10-04 15:09:24 +0800

    P147-149

--- lab10_vue3_cli/src/App.vue
+++ lab10_vue3_cli/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>這是主程式</h1>
+    <h2>{{ title }}</h2>
+    <course-info></course-info>
+    <course-info></course-info>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: "First Vue CLI program"
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ lab10_vue3_cli/src/components/CourseIntro.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>單一課程簡介</h1>
+    <h2>{{ course.id }}</h2>
+    <button @click="toggleCourseDetail">show detail</button>
+    <ul v-if="detailsVisible">
+      <li>{{ course.name }}</li>
+      <li>{{ course.duration }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      course: { id: "POOP", name: "Python OOP", duration: "35hr" },
+      detailsVisible: true
+    }
+  },
+  methods: {
+    toggleCourseDetail() {
+      this.detailsVisible = !this.detailsVisible;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
