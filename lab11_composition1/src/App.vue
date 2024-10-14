441279a4ddc78742eaf7f8f7a0392f620ff3981b
genjet <gengywang@gmail.com>
2024-10-14 13:27:41 +0800

    P211-212

--- lab11_composition1/src/App.vue
+++ lab11_composition1/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <section>
+    <h1>會reactive</h1>
     <h3>full user object:{{ user }}</h3>
     <h3>get name by user:{{ user.userName }}</h3>
     <h3>get age by user:{{ user.age }}</h3>
+    <hr />
+    <h1>固定值的</h1>
     <h3>user={{ userName }},age={{ age }}</h3>
+    <h3>courseName={{ courseName }},duration={{ duration }}</h3>
   </section>
 </template>
 
 <script>
-import { ref, reactive, isReactive, isRef } from 'vue'
+import { ref, reactive, isReactive, isRef, toRefs } from 'vue'
 export default {
   name: 'App',
   setup() {
@@ -18,6 +22,17 @@ export default {
       age: 48
     }
     const user = reactive(initUser)
+    const course = reactive({ courseName: 'poop', duration: 35 })
+    const courseRef = toRefs(course)
+    console.log(course)
+    console.log(courseRef)
+    // course.duration += 7
+    // console.log("reactive object course:", course.duration)
+    // console.log("reactive object property to Ref:", courseRef.duration.value)
+    // courseRef.duration.value += 7
+    // console.log("reactive object course:", course.duration)
+    // console.log("reactive object property to Ref:", courseRef.duration.value)
+    // console.log("********************")
     console.log(currentAge, user)
     console.log(currentAge.value, user.age, user.userName)
     console.log(`currentAge is a reactive variable? ${isReactive(currentAge)}`)
@@ -27,8 +42,25 @@ export default {
     setTimeout(function () {
       user.userName = "Meng-Hang Ho"
       user.age += 1
+      course.courseName = "Python OOP"
+      course.duration += 7
+      console.log(`修改完是:${course.courseName},${course.duration}`)
+      console.log(`[ref]修改完是:${courseRef.courseName.value},${courseRef.duration.value}`)
+      setTimeout(() => {
+        courseRef.courseName.value = "Python物件導向"
+        courseRef.duration.value += 7
+        console.log(`修改完是:${course.courseName},${course.duration}`)
+        console.log(`[ref]修改完是:${courseRef.courseName.value},${courseRef.duration.value}`)
+
+      }, 1000)
     }, 2000)
-    return { user: user, userName: user.userName, age: user.age }
+    // return {
+    //   user: user, userName: user.userName, age: user.age, courseName: course.courseName,
+    //   duration: course.duration
+    // }
+    return {
+      user: user, userName: user.userName, age: user.age, ...toRefs(course)
+    }
   }
 }
 </script>
