0164719093c183ae0db46a1bd0cf7d34277700e1
genjet <gengywang@gmail.com>
2024-10-14 11:05:10 +0800

    p203-204

---
+++ lab11_composition1/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h3>{{ userName }}</h3>
+  </section>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    console.log("init composition API")
+    const initName = "MarkHo"
+    const currentName = ref(initName)
+    console.log(initName)
+    console.log(currentName)
+    return {userName:currentName}
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
