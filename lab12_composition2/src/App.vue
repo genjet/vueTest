d27168667c17b5987097cdf1751e4cdb909e25b9
genjet <gengywang@gmail.com>
2024-10-14 14:37:54 +0800

    p226-227

--- lab12_composition2/src/App.vue
+++ lab12_composition2/src/App.vue
@@ -2,35 +2,45 @@
   <h1>composition函數</h1>
   <h2>{{ course }}/{{ price }}</h2>
   <h3>{{ courseDisplayFullName }}</h3>
-  <button @click="course.duration += 7">時數加1天(bad)</button>
+  <button @click="course.duration+=7">時數加1天(bad)</button>
   <button @click="extraDuration">時數加1天</button>
-  <hr />
-  <input type="text" placeholder="courseId" v-model="courseId" />
-  <input type="text" placeholder="courseFullName" v-model="courseFullName" />
+  <hr/>
+  <input type="text" v-model="courseId"/>
+  <input type="text" v-model="courseFullName"/>
 </template>
 
 <script>
-import { ref, reactive, computed } from 'vue'
+import { ref,reactive,computed,watch } from 'vue'
 export default {
   name: 'App',
   setup() {
-    const courseId = ref("")
-    const courseFullName = ref("")
-    const courseDisplayFullName = computed(function () {
+    const courseId = ref("bdpy")
+    const courseFullName = ref("python and big data")
+    const courseDisplayFullName = computed(function(){
       console.log("calculate full name")
       return `[${courseId.value}]${courseFullName.value}`
     })
     const price = ref(24000)
+    // moniter value changes
+    watch([price,courseDisplayFullName],function(newValue,oldValue){
+      if (oldValue[0]!==newValue[0]) {
+        console.log(`價格由${oldValue[0]}到${newValue[0]}`)
+      }
+      if (oldValue[1]!==newValue[1]) {
+        console.log(`課名由${oldValue[1]}到${newValue[1]}`)
+      }
+      
+    })
     const course = reactive({ name: "POOP", duration: 35 })
-    function extraDuration() {
+    function extraDuration(){
       course.duration += 7
       price.value += 5000
     }
-    
-    return {
-      course: course, extraDuration: extraDuration, price: price,
-      courseDisplayFullName: courseDisplayFullName, courseId: courseId,
-      courseFullName: courseFullName
+
+
+    return {course:course, extraDuration:extraDuration,price:price,
+      courseDisplayFullName:courseDisplayFullName,courseId:courseId,
+      courseFullName:courseFullName
     }
   }
 }
