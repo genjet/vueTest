d121225b60cbd5d6af10ca418547b76a00bbee3e
genjet <gengywang@gmail.com>
2024-10-14 16:32:05 +0800

    onUnmounted

--- lab12_composition2/src/App.vue
+++ lab12_composition2/src/App.vue
@@ -1,61 +1,70 @@
 <template>
   <h1>composition函數</h1>
-  <course-intro
-  :courseId="courseId"
-  :courseFullName="courseFullNameInput">
-  </course-intro>
+  <button @click="toggleCourseDisplay">toggle course</button>
+  <div v-if="displayCourse">
+    <course-intro :courseId="courseId" :courseFullName="courseFullNameInput"></course-intro>
+  </div>
   <h3>{{ courseDisplayFullName }}</h3>
-  <button @click="course.duration+=7">時數加1天(bad)</button>
+  <button @click="course.duration += 7">時數加1天(bad)</button>
   <button @click="extraDuration">時數加1天</button>
-  <hr/>
-  <input type="text" v-model="courseId"/>
-  <input type="text" v-model="courseFullName"/>
-  <hr/>
-  <input type="text" ref="courseFullNameInput"/>
+  <hr />
+  <input type="text" v-model="courseId" />
+  <input type="text" v-model="courseFullName" />
+  <hr />
+  <input type="text" ref="courseFullNameInput" />
   <button @click="setCourseFullName">set course full name</button>
 </template>
 
 <script>
-import { ref,reactive,computed,watch, provide  } from 'vue'
+import { ref, reactive, computed, watch, provide } from 'vue'
 import CourseIntro from './components/CourseIntro.vue';
 export default {
-  components:{CourseIntro},
+  components: { CourseIntro },
   name: 'App',
   setup() {
+    // display related
+    const displayCourse = ref(true)
+    function toggleCourseDisplay() {
+      displayCourse.value = !displayCourse.value
+    }
+    // course related
     const courseId = ref("")
     const courseFullName = ref("")
     const courseFullNameInput = ref()
     function setCourseFullName() {
       courseFullName.value = courseFullNameInput.value.value
     }
-    const courseDisplayFullName = computed(function(){
+    const courseDisplayFullName = computed(function () {
       console.log("calculate full name")
       return `[${courseId.value}]${courseFullName.value}`
     })
     const price = ref(24000)
-    provide("price",price)
+    provide("price", price)
     // moniter value changes
-    watch([price,courseDisplayFullName],function(newValue,oldValue){
-      if (oldValue[0]!==newValue[0]) {
+    watch([price, courseDisplayFullName], function (newValue, oldValue) {
+      if (oldValue[0] !== newValue[0]) {
         console.log(`價格由${oldValue[0]}到${newValue[0]}`)
       }
-      if (oldValue[1]!==newValue[1]) {
+      if (oldValue[1] !== newValue[1]) {
         console.log(`課名由${oldValue[1]}到${newValue[1]}`)
       }
-      
+
     })
     const course = reactive({ name: "POOP", duration: 35 })
-    function extraDuration(){
+    function extraDuration() {
       course.duration += 7
       price.value += 5000
     }
 
 
-    return {course:course, extraDuration:extraDuration,price:price,
-      courseDisplayFullName:courseDisplayFullName,courseId:courseId,
-      courseFullName:courseFullName,
-      courseFullNameInput:courseFullNameInput,
-      setCourseFullName:setCourseFullName
+    return {
+      course: course, extraDuration: extraDuration, price: price,
+      courseDisplayFullName: courseDisplayFullName, courseId: courseId,
+      courseFullName: courseFullName,
+      courseFullNameInput: courseFullNameInput,
+      setCourseFullName: setCourseFullName,
+      displayCourse: displayCourse,
+      toggleCourseDisplay: toggleCourseDisplay
     }
   }
 }
