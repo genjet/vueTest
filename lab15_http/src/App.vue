e7d09f06a800cfbdfc52822b2cf531808157edc1
genjet <gengywang@gmail.com>
2024-10-15 11:10:23 +0800

    p320-321

--- lab15_http/src/App.vue
+++ lab15_http/src/App.vue
@@ -1,15 +1,21 @@
 <template>
+  <local-access></local-access>
+  <internal-access></internal-access>
   <firebase-access></firebase-access>
 </template>
 
 <script>
 import FirebaseAccess from './components/FirebaseAccess.vue';
+import InternalAccess from './components/InternalAccess.vue';
+import LocalAccess from './components/LocalAccess.vue';
 
 
 export default {
 
   components: {
-    FirebaseAccess
+    FirebaseAccess,
+    LocalAccess,
+    InternalAccess
   }
 }
 </script>
--- lab15_http/src/components/FirebaseAccess.vue
+++ lab15_http/src/components/FirebaseAccess.vue
@@ -2,15 +2,20 @@
     <div>
         <h1>get data from firebase</h1>
         <button @click="send1">send data using built-in fetch</button>
+        <br>
+        <button @click="send2">post using axios</button>
     </div>
 </template>
 
 <script>
 const URL1 = "https://vuetest202410-default-rtdb.firebaseio.com/lab15/course.json"
+const URL2 = "https://vuetest202410-default-rtdb.firebaseio.com/lab15/instructors.json"
+import axios from 'axios';
 export default {
     data() {
         return {
             course: { id: "POOP", name: "Python OOP", duration: 35 },
+            instructor:{name:"Ken",grade:"senior"},
             courses: []
         }
     },
@@ -23,18 +28,31 @@ export default {
                 },
                 body: JSON.stringify(this.course)
             })
-            .then(response => {
-                console.log("response is:", response)
-                if (response.ok) {
-                    const data = response.json();
-                    console.log("response json:", data)
-                    return data
+                .then(response => {
+                    console.log("response is:", response)
+                    if (response.ok) {
+                        const data = response.json();
+                        console.log("response json:", data)
+                        return data
+                    }
+                })
+                .then(data => {
+                    console.log("最後取得的結果:", data)
+                })
+        },
+        send2() {
+            axios.post(URL2, this.instructor).then(
+                response => {
+                    console.log("response=", response)
+                    if (response.status == 200) {
+                        return response.data
+                    }
                 }
+            ).then(data => {
+                console.log("axios取得的結果是", data)
             })
-            .then(data => {
-                console.log("最後取得的結果:", data)
-            })
-        }
+
+        }   
     }
 }
 </script>
