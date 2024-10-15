25ed41958000abadcf17535ed364fd303574bd30
genjet <gengywang@gmail.com>
2024-10-15 11:08:35 +0800

    script setup

---
+++ lab15_http/src/components/InternalAccess.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>vbase3 setup</h1>
+        <h2>{{ course }}</h2>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const course=ref({id:"POOP",name:"Python OOP",duration:35})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ lab15_http/src/components/LocalAccess.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>vbase3</h1>
+        <h2>{{ course }}</h2>
+    </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+    setup () {
+        const course=ref({id:"POOP",name:"Python OOP",duration:35})
+
+        return {course}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
