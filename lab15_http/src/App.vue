83486c6db9f98b1058bf045d4a19671f186eddf8
genjet <gengywang@gmail.com>
2024-10-15 10:28:00 +0800

    rest client +&P312-319

---
+++ lab15_http/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <firebase-access></firebase-access>
+</template>
+
+<script>
+import FirebaseAccess from './components/FirebaseAccess.vue';
+
+
+export default {
+
+  components: {
+    FirebaseAccess
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lab15_http/src/components/FirebaseAccess.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>get data from firebase</h1>
+        <button @click="send1">send data using built-in fetch</button>
+    </div>
+</template>
+
+<script>
+const URL1 = "https://vuetest202410-default-rtdb.firebaseio.com/lab15/course.json"
+export default {
+    data() {
+        return {
+            course: { id: "POOP", name: "Python OOP", duration: 35 },
+            courses: []
+        }
+    },
+    methods: {
+        send1() {
+            fetch(URL1, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(this.course)
+            })
+            .then(response => {
+                console.log("response is:", response)
+                if (response.ok) {
+                    const data = response.json();
+                    console.log("response json:", data)
+                    return data
+                }
+            })
+            .then(data => {
+                console.log("最後取得的結果:", data)
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
